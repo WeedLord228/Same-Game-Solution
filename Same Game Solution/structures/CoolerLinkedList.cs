f86301f9f32321156bfc3dc434e487a1f9ddda71
jubick1337 <mattyson.so@gmail.com>
2021-05-09 20:05:14 +0500

    coolerll update

--- Same Game Solution/structures/CoolerLinkedList.cs
+++ Same Game Solution/structures/CoolerLinkedList.cs
@@ -5,8 +5,8 @@ namespace Same_Game_Solution.structures
 {
     public class CoolerLinkedList<T> : ICollection<T>
     {
-        private LinkedList<T> _hiddenList = new LinkedList<T>();
-        private Dictionary<int, LinkedList<T>> _hiddenDict = new Dictionary<int, LinkedList<T>>();
+        private LinkedList<T> _hiddenList = new();
+        private Dictionary<int, LinkedList<T>> _hiddenDict = new();
 
         public IEnumerator<T> GetEnumerator()
         {
@@ -24,12 +24,9 @@ namespace Same_Game_Solution.structures
             LinkedList<T> currentList;
             if (_hiddenDict.TryGetValue(hash, out currentList))
             {
-                foreach (var val in currentList)
-                {
-                    if (val.Equals(item)) continue;
-                    currentList.AddLast(item);
-                    _hiddenList.AddLast(item);
-                }
+                if (currentList.Contains(item)) return;
+                currentList.AddLast(item);
+                _hiddenList.AddLast(item);
             }
             else
             {
